c0688641d8578366aef5b3c25b7e477930582925
Sufiyan123yivh <farhanrehan2002@gmail.com>
2025-10-22 14:11:22 +0530

    Update Tvzon.js

--- api/Tvzon.js
+++ api/Tvzon.js
@@ -4,7 +4,7 @@ import path from "path";
 
 // ⚙️ Configuration
 const config = {
-  url: "https://tatatv.cc/stalker_portal/c/",
+  url: "tatatv.cc:80",
   mac: " 00:1A:79:00:13:DA",
   sn: "8DC34D20E1021",
   device_id_1:
